97c8fb9ba327aef66a563d9656ae916a51963c9b
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-22 10:14:06 -0700

    Done.

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+// src/components/App.js
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
